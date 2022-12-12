c6fd5aa2726c729af5f56efffcb15a26db418316
ProgHazem <35069122+ProgHazem@users.noreply.github.com>
2022-12-12 11:34:29 +0200

    remove unimportant file

--- HazemController.ts
+++
@@ -1,9 +0,0 @@
-
-      import { inject } from '@adonisjs/fold'
-      import HazemService from 'App/Modules/Hazem/Services/HazemService'
-
-      @inject()
-      export default class HazemController {
-
-        constructor(private hazemService: HazemService) {}
-      }
